0fff3204d9fcf54cf672b474b817ee145a17af1d
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-03-18 02:06:09 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,39 +3,74 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    
+    last_parent_index = len(data) // 2 - 1
+    for i in range(last_parent_index, -1, -1):
+        heapify(data, i, swaps)
+        
     return swaps
 
 
-def main():
+def heapify(data, index, swaps):
+    smaller_index = index
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    left_child_index = 2 * index + 1
+    if (left_child_index < len(data) and data[left_child_index] < data[smaller_index]):
+        smaller_index = left_child_index
+    
+    right_child_index = 2 * index + 2
+    if (right_child_index < len(data) and data[right_child_index] < data[smaller_index]):
+        smaller_index = right_child_index
+        
+    if (smaller_index == index):
+        return
+    
+    swaps.append([index,smaller_index])
+    
+    temp = data[index]
+    data[index] = data[smaller_index]
+    data[smaller_index] = temp
+    
+    heapify(data, smaller_index, swaps)
+    
+    
+def main():
+    command = input()
+    if "F" in command:
+        file_name = input()
+        path = "test/" + file_name
+        if not "a" in file_name:
+            contents = open(path, "r")
+            text = contents.read()
+            contents.close()
+            partitioned = text.partition("\n")
+            n = int(partitioned[0])
+            data = partitioned[2].split(" ")
+            assert len(data) == n
+            
+            swaps = build_heap(data)
+            
+            if (len(swaps) >= 4*len(data)):
+                return
+      
+            print(len(swaps))
+            for i, j in swaps:
+                print(i, j)
+            
+        
+    elif "I" in command:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        
+        swaps = build_heap(data)
+        
+        if (len(swaps) >= 4*len(data)):
+            return
+        
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
 
 
 if __name__ == "__main__":
